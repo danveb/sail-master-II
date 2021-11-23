7d4f5b29289a0e1d259be35602cbc8fe802f5918
Daniel Bae <danibae91@icloud.com>
2021-11-23 14:24:33 -0800

    clean up VoyageList to include Table to display voyage, and remove VoyageTable component

--- frontend/src/components/VoyageList.js
+++ frontend/src/components/VoyageList.js
@@ -1,47 +1,60 @@
 import React, { useState, useEffect } from 'react' 
-import VoyageTable from './VoyageTable'
 import SailMasterIIApi from '../API/api'
 import { Link } from 'react-router-dom'
-import { Container, Row, Button } from 'react-bootstrap' 
+import { Container, Row, Button, Table } from 'react-bootstrap'
+import { FaCheckCircle, FaTrashAlt } from "react-icons/fa"
 
 const VoyageList = ({ currentUser }) => {    
     // useState
-    const [voyages, setVoyages] = useState([])
+    const [voyage, setVoyage] = useState([])
 
     // useEffect
     useEffect(() => {
         async function getVoyages() {
             try {
                 let voyages = await SailMasterIIApi.getVoyages() 
-                setVoyages(voyages) 
+                setVoyage(voyages) 
             } catch(err) {
                 console.error(err)
             }
         }
         getVoyages() 
-    }, [setVoyages])
+    }, [setVoyage])
 
     // filter voyages based on currentUser
     // if currentUser.isAdmin can view all voyages
-    let filteredVoyages = voyages.filter((v) => v.sailorUsername === currentUser.username || currentUser.isAdmin)
+    let filteredVoyages = voyage.filter((v) => v.sailorUsername === currentUser.username || currentUser.isAdmin)
 
-    if(!voyages) return console.error('No voyages yet. Please consider adding one')
+    if(!voyage) return console.error('No voyages yet. Please consider adding one')
 
     return (
         <Container className="d-flex align-items-center justify-content-center mt-4">
-            <Row>
-                <h1 className="text-center mb-4">Voyage List</h1>
-                <Button as={Link} to={`/voyage/new`} variant="success" className="mb-4">Create A Voyage</Button>
+        <Row>
+            <h1 className="text-center mb-4">Voyage List</h1>
+            <Button as={Link} to={`/voyage/new`} variant="success" className="mb-4">Create A Voyage</Button>
+                <Table striped bordered hover size="s">
+                <thead>
+                    <tr>
+                        <th width="100">Start Point</th>
+                        <th width="100">End Point</th>
+                        <th width="100">@username</th>
+                        <th width="100">View</th>
+                        <th width="100">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
                 { filteredVoyages.map((voyage) => (
-                    <VoyageTable
-                        key={voyage.id}
-                        id={voyage.id}
-                        startPoint={voyage.startPoint}
-                        endPoint={voyage.endPoint}
-                        sailorUsername={voyage.sailorUsername} 
-                    />
-                ))} 
-            </Row>
+                    <tr>
+                        <td>{voyage.startPoint}</td>
+                        <td>{voyage.endPoint}</td>
+                        <td>{voyage.sailorUsername}</td>
+                        <td><Button variant="warning" as={Link} to={`/voyage/${voyage.id}`}><FaCheckCircle /></Button></td>
+                        <td><Button variant="danger" onClick={() => SailMasterIIApi.removeVoyage(voyage.id)}><FaTrashAlt /></Button></td>
+                    </tr>
+                ))}
+                </tbody>
+                </Table>
+        </Row>
         </Container>
     )
 }
