6e3790cdab5f065642c8e1841a34ce43fd18be45
Daniel Bae <danibae91@icloud.com>
2021-11-23 22:08:02 -0800

    add tests for voyage/user models

--- backend/models/voyage.test.js
+++ backend/models/voyage.test.js
@@ -20,22 +20,49 @@ afterAll(commonAfterAll);
 
 /************************************** */
 
-describe("create a new voyage", function () {
-	test("creates new voyage successfully", async function () {
+describe("create a new voyage", function() {
+	test("creates new voyage successfully", async function() {
 		let voyage = await Voyage.create(newVoyage) 
 		expect(voyage).toEqual({
 			...newVoyage,
 			id: expect.any(Number)
-		});
-	});
-});
+		})
+	})
+})
 
-describe("find all voyage in the database", function () {
-	test("finds all voyage in db", async function () {
+describe("find all voyage in the database", function() {
+	test("finds all voyage in db", async function() {
 		let voyage = await Voyage.findAll();
 
 		expect(voyage).toEqual([
 			{ ...voyage1, id: testVoyageIds[0] }
-		]);
-	});
-});
+		])
+	})
+})
+
+describe("find voyage based on id", function() {
+	test("find voyage per id", async function() {
+		let voyage = await Voyage.get(testVoyageIds[0])
+		expect(voyage).toEqual({
+			...voyage1, 
+			id: testVoyageIds[0]
+		})
+	})
+
+	test("did not find voyage per id", async function() {
+		try {
+			await Voyage.get(0)
+			fail()
+		} catch(err) {
+			expect(err instanceof NotFoundError).toBeTruthy()
+		}
+	})
+})
+
+describe("remove a voyage using id", function() {
+	test("works when removing a voyage", async function() {
+		await Voyage.remove(testVoyageIds[0])
+		const res = await db.query('DELETE FROM voyage WHERE id=$1', [testVoyageIds[0]])
+		expect(res.rows.length).toEqual(0) 
+	})
+})
